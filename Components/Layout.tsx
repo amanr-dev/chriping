f53b13b4c81b19c44021af3f86111b319b604c98
Aman <dv451197@gmail.com>
2024-04-05 17:37:08 +0530

    Updating Layout

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "./Sidebar";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -7,6 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screeh bg-slate-900">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
+          <Sidebar />
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-x-slate-800">
             {children}
           </div>
@@ -15,3 +18,5 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     </div>
   );
 };
+
+export default Layout;
