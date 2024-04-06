9afbeb86d99152f9212043384ab715bbf6284b67
Aman <dv451197@gmail.com>
2024-04-06 22:31:17 +0530

    added some spices

--- Components/layout/RightSidebar.tsx
+++ Components/layout/RightSidebar.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+
+const links = [
+  {
+    label: "Following",
+    href: "/following",
+  },
+  {
+    label: "Discover",
+    href: "/",
+  },
+  {
+    label: "For You",
+    href: "/",
+  },
+];
 
 export const RightSidebar = () => {
   return (
@@ -12,10 +28,18 @@ export const RightSidebar = () => {
           placeholder="Search"
         />
       </div>
-      <div className="w-full h-[1px] bg-slate-800 my-2" />
+      <div className="w-full h-[1px] bg-slate-800 my-4" />
+      <div className="w-full flex items-start justify-start flex-col text-primary capitalize gap-2 text-xl">
+        {links.map((link) => (
+          <Link href={link.href} key={link.label}>
+            {link.label}
+          </Link>
+        ))}
+      </div>
+      <div className="w-full h-[1px] bg-slate-800 my-4" />
       <div className="bg-slate-800 rounded-xl p-4 mt-6">
         <h2 className="text-primary text-xl font-semibold">Who to Follow</h2>
-        <div className="flex flex-col gap-6 mt-4">{/* ADD USER LIST */}</div>
+        <div className="flex flex-col gap-6 mt-4"></div>
       </div>
     </div>
   );
