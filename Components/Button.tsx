8b8625955c44b472208a870433cd089c41311496
Aman <dv451197@gmail.com>
2024-04-07 09:49:45 +0530

    built Button and Modal

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -19,5 +19,31 @@ export const Button: React.FC<ButtonProps> = ({
   secondary,
   outline,
 }) => {
-  return <div>Button</div>;
+  return (
+    <button
+      disabled={disabled}
+      onClick={onclick}
+      className={`
+              disabled:opacity-70
+              disabled:cursor-not-allowed
+              rounded-full
+              font-semibold
+              hover:opacity-80
+              transition
+              border-2
+              ${fullWidth ? "w-full" : "w-fit"}
+              ${secondary ? "bg-primary" : "bg-secondary"}
+              ${secondary ? "text-slate-800" : "text-primary"}
+              ${secondary ? "border-slate-800" : "border-blue-300"}
+              ${large ? "text-xl" : "text-base"}
+              ${large ? "px-5" : "px-4"}
+              ${large ? "py-3" : "px-2"}
+              ${outline ? "bg-transparent" : ""}
+              ${outline ? "border-blue-50" : ""}
+              ${outline ? "text-primary" : ""}
+              `}
+    >
+      {label}
+    </button>
+  );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Layout from "@/Components/layout/Layout";
+import { Modal } from "@/Components/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout>{children}</Layout>
+        <Layout>
+          <Modal />
+          {children}
+        </Layout>
       </body>
     </html>
   );
