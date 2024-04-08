a880034b6161bce15568168fd23e4c020ceb446d
Aman <dv451197@gmail.com>
2024-04-08 22:24:13 +0530

    main frontend done now backend

--- Components/layout/SidebarTweetButton.tsx
+++ Components/layout/SidebarTweetButton.tsx
@@ -1,16 +1,26 @@
 "use client";
 
+import useLoginModal from "@/hooks/useLoginModal";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 
 export const SidebarTweetButton = () => {
+  const loginModal = useLoginModal();
+
+  const toggleModal = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   const router = useRouter();
+
   return (
-    <div onClick={() => router.push("/")}>
+    <div onClick={toggleModal}>
       <div
         className="
-  mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-secondary hover:bg-opacity-10  cursor-pointer transition text-primary hover:text-primary
+  
+        mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-secondary hover:bg-opacity-10  cursor-pointer transition text-primary hover:text-primary
+  
   "
       >
         <FaFeather size={24} />
@@ -20,11 +30,11 @@ export const SidebarTweetButton = () => {
       mt-6 hidden lg:block px-3 py-2 rounded-full bg-secondary hover:opacity-90 cursor-pointer transition
       "
       >
-        <div className="flex items-center justify-center flex-row w-full gap-4">
+        <div className="flex items-center justify-center flex-row w-full gap-4 text-primary">
           <FaFeather size={24} />
           <p
             className="
-          hidden lg:block text-center font-semibold text-slate-900 text-[18px]
+          hidden lg:block text-center font-semibold text-blue-50 text-[18px]
           "
           >
             Write
--- hooks/useRegisterModal.ts
+++ hooks/useRegisterModal.ts
@@ -7,7 +7,7 @@ interface RegisterModal {
 }
 
 const useRegisterModal = create<RegisterModal>((set) => ({
-  isOpen: true,
+  isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
