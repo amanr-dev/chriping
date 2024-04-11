c12b25b241c2d6d52bfdc6cc0ec85a8ce1f91c69
Aman <dv451197@gmail.com>
2024-04-11 15:06:28 +0530

    some scretes

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,1 +1,5 @@
-// TODO install bcrypt, -D @types/bcrypt, next-auth, @next-auth/prisma-adapter
+import bcrypt from "bcrypt";
+import NextAuth from "next-auth";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/libs/prismadb";
