9ac37b142bc8c644b1e486c9fd606b9a43495ad0
Aman <dv451197@gmail.com>
2024-04-12 14:46:03 +0530

    adding create account functionality

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -4,6 +4,8 @@ import fetcher from "@/libs/fetcher";
 
 const useCurrentUser = () => {
   const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher);
+
+  console.log({ data, error, isLoading });
 };
 
 export default useCurrentUser;
