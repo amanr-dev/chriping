a0322fbca0246ecdd90c1180b40dff896746cc90
Aman <dv451197@gmail.com>
2024-04-22 22:16:27 +0530

    creating auth.ts

--- auth.ts
+++ auth.ts
@@ -1,16 +1,22 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 // Your own logic for dealing with plaintext password strings; be careful!
-import { saltAndHashPassword } from "@/utils/password";
+// import { saltAndHashPassword } from "@/utils/password";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 export const { handlers, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma)
   providers: [
     Credentials({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        email: {},
-        password: {},
+        email: {
+          label: "email",
+          type: "text",
+        },
+        password: { label: "password", type: "password" },
       },
       authorize: async (credentials) => {
         let user = null;
