b214075b122c7b76c7542fcd7f81f745d41ff86b
Aman <dv451197@gmail.com>
2024-04-23 23:09:18 +0530

    dependency conflict

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,9 @@
 import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import prisma from "@/libs/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
