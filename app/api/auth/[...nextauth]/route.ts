4f0b24190c669ee034ab427da21f45d89dda064d
Aman <dv451197@gmail.com>
2024-04-23 23:02:37 +0530

    deleted authjs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,57 +1,54 @@
-// "use server";
+"use server";
 
-// import bcrypt from "bcrypt";
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import prisma from "@/libs/prismadb";
+import bcrypt from "bcrypt";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/libs/prismadb";
 
-// export default NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {
-//         email: { label: "email", type: "text" },
-//         password: { label: "password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials.password) {
-//           throw new Error("Invalid Credentials");
-//         }
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid Credentials");
+        }
 
-//         const user = await prisma.user.findUnique({
-//           where: {
-//             email: credentials.email,
-//           },
-//         });
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-//         if (!user || !user?.hashedPassword) {
-//           throw new Error("Invalid Credentials");
-//         }
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid Credentials");
+        }
 
-//         const isCorrectPassword = await bcrypt.compare(
-//           credentials.password,
-//           user.hashedPassword
-//         );
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
 
-//         if (!isCorrectPassword) {
-//           throw new Error("Invalid Password");
-//         }
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Password");
+        }
 
-//         return user;
-//       },
-//     }),
-//   ],
-//   debug: process.env.NODE_ENV === "development",
-//   session: {
-//     strategy: "jwt",
-//   },
-//   jwt: {
-//     secret: process.env.NEXTAUTH_JWT_SECRET,
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
-
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-export const { GET, POST } = handlers;
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
