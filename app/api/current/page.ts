bfaa3ca82728cabbc831237b64140547e2ed22f7
Aman <dv451197@gmail.com>
2024-04-24 18:44:22 +0530

    added getCurrentUser function

--- app/api/current/page.ts
+++ app/api/current/page.ts
@@ -7,9 +7,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // if (req.method !== "GET") {
-  //   return res.status(405).end();
-  // }
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
 
   try {
     const { currentUser } = await serverAuth(req);
