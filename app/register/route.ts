e6ce26948d8f62d15622f67b3b93d586c8e11044
Aman <dv451197@gmail.com>
2024-05-02 23:18:04 +0530

    updated register route

--- app/register/route.ts
+++ app/register/route.ts
@@ -1,39 +1,60 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     return res.status(405).end();
+//   }
 
-  const { email, username, name, password } = req.body;
+//   const { email, username, name, password } = req.body;
 
-  try {
-    console.log("Request Body:" + req.body);
+//   try {
+//     console.log("Request Body:" + req.body);
 
-    if (!email || !username || !name || !password) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
+//     if (!email || !username || !name || !password) {
+//       return res.status(400).json({ error: "Missing required fields" });
+//     }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+//     const hashedPassword = await bcrypt.hash(password, 12);
 
-    // Create User to db
-    const user = await prisma.user.create({
-      data: {
-        email,
-        username,
-        name,
-        hashedPassword,
-      },
-    });
+//     // Create User to db
+//     const user = await prisma.user.create({
+//       data: {
+//         email,
+//         username,
+//         name,
+//         hashedPassword,
+//       },
+//     });
 
-    return res.status(200).json(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).end();
-  }
+//     return res.status(200).json(user);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(400).end();
+//   }
+// }
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const { email, username, name, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  // Create User to db
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }
