35b2253b070e982e923aba52acf603cba2efd54b
Aman <dv451197@gmail.com>
2024-05-04 23:14:29 +0530

    fix type error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
 const page = () => {
+  const session = useSession();
+  useEffect(() => {
+    if (!session.data) {
+      console.log("No Session");
+    } else {
+      console.log(session);
+    }
+  }, []);
+
   return "Suxxy Baka";
 };
 
