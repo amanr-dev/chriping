32c82e7d8c2a6539fd116b216b4f6db866023669
Aman <dv451197@gmail.com>
2024-06-15 17:55:40 +0530

    uncommented credentials

--- auth.ts
+++ auth.ts
@@ -5,26 +5,27 @@ import prisma from "@/libs/prismadb";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    // Credentials({
-    //   credentials: {
-    //     email: {},
-    //     password: {},
-    //   },
-    //   authorize: async (credentials: any) => {
-    //     try {
-    //       const pwHash = bcrypt.hash(credentials?.password, 10);
-    //       // logic to verify if user exists
-    //       const user = { credentials, pwHash };
-    //       if (!user) {
-    //         throw new Error("User not found.");
-    //       }
-    //       // return json object with the user data
-    //       return user;
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   },
-    // }),
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials: any) => {
+        try {
+          const pwHash = bcrypt.hash(credentials?.password, 10);
+          // logic to verify if user exists
+          const user = { credentials, pwHash };
+          if (!user) {
+            throw new Error("User not found.");
+          }
+          // return json object with the user data
+          return user;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
   ],
+
   secret: process.env.NEXT_AUTH_SECRET,
 });
