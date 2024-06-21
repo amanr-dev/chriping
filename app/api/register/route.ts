ce33df9be53d71a3d16c0bf97226b1934373c423
Aman <dv451197@gmail.com>
2024-06-21 22:07:34 +0530

    creating register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { email, username, password, name } = req.body;
+  } catch (error) {
+    console.log(error);
+  }
+}
