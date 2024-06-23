80b4b0fe6c8300c9e60770cba45d7a955e3650ce
Aman <dv451197@gmail.com>
2024-06-23 21:33:10 +0530

    done register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/libs/prismadb";
 import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
@@ -19,5 +20,5 @@ export async function POST(req: Request) {
   });
   console.log(user);
 
-  return user;
+  return NextResponse.json(user);
 }
