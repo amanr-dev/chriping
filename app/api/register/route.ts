ae1a2de293f5b3fb5ff14e6e6969ae9e006e005b
Aman <dv451197@gmail.com>
2024-06-23 21:30:08 +0530

    removed "username" from register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,23 +4,20 @@ import bcrypt from "bcrypt";
 
 export async function POST(req: Request) {
   const body = await req.json();
-  try {
-    const { email, username, password, name } = body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+  const { email, password, name } = body;
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        name,
-        username,
-        hashedPassword,
-      },
-    });
-    console.log(user);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-    return user;
-  } catch (error) {
-    console.log(error);
-  }
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+
+      hashedPassword,
+    },
+  });
+  console.log(user);
+
+  return user;
 }
