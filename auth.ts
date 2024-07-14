88db9ad4435226e9fccad5ca9e709c1c2d154a6f
Aman <dv451197@gmail.com>
2024-07-14 23:15:24 +0530

    adding credentials login

--- auth.ts
+++ auth.ts
@@ -1,10 +1,31 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import credentials from "next-auth/providers/credentials";
+import prisma from "@/libs/prismadb";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    credentials({
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials || typeof credentials.username !== "string") {
+          return null;
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            username: credentials.username,
+          },
+        });
+        if (user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 });
